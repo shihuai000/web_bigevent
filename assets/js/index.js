5baf85d3b0e2f9804e9a871d3e2e6e19293e93ff
shiwei <1723633850@qq.com>
2020-12-07 14:26:42 +0800

    index分支完成

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+$(function() {
+    var layer = layui.layer
+    getuserinfo()
+
+    $(".btnlogout").on("click", function() {
+        layer.confirm("确定退出登录？", { icon: 3, title: "提示" }, function(index) {
+            localStorage.removeItem("token")
+            location.href = "/login.html"
+            layer.close(index)
+        })
+    })
+
+})
+
+
+function getuserinfo() {
+    $.ajax({
+        method: "get",
+        url: "/my/userinfo",
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg("获取用户信息失败")
+            }
+            renderAvatar(res.data)
+        },
+
+
+    })
+
+    function renderAvatar(user) {
+        var name = user.nickname || user.username
+        $(".welcome").html("欢迎&nbsp&nbsp" + name)
+        if (user.user_pic !== null) {
+            $(".text-avatar").hide()
+            $(".layui-nav-img").attr('src', user.user_pic).show()
+        } else {
+            $(".layui-nav-img").hide()
+            var first = name[0].toUpperCase()
+            $(".text-avatar").html(first).show()
+        }
+    }
+}
